41a5a132b0a6877de399d0445a99d0fbe9514439
Sai Kasam <saikasam@sais-mbp.attlocal.net>
2017-12-20 18:44:32 -0600

    switch to amam

--- HQ.py
+++ HQ.py
@@ -57,24 +57,18 @@ def get_ans(question, answers):
 
 def get_text():
     api_key = 'AIzaSyD62V5CUucbPUnx21i-cQvKS9cOngm2eeI'
-    image_filenames = ['imageTest4.png']
-    if not api_key or not image_filenames:
+    image_filename = ['imageTest.png']
+    if not api_key or not image_filename:
         print("""
             Please supply an api key, then one or more image filenames
             $ python cloudvisreq.py api_key image1.jpg image2.png""")
     else:
-        response = request_ocr(api_key, image_filenames)
+        response = request_ocr(api_key, image_filename)
         if response.status_code != 200 or response.json().get('error'):
             print(response.text)
         else:
             for idx, resp in enumerate(response.json()['responses']):
-                #save to JSON file
-                imgname = image_filenames[idx]
-                jpath = join(RESULTS_DIR, basename(imgname) + '.json')
-                with open(jpath, 'w') as f:
-                    datatxt = json.dumps(resp, indent=2)
-                    print("Wrote", len(datatxt), "bytes to", jpath)
-                    f.write(datatxt)
+
 
                 # print the plaintext to screen for convenience
                 t = resp['textAnnotations'][0]
@@ -91,14 +85,14 @@ def get_text():
                 print()
                 get_ans(question, answers)
 
-def make_image_data_list(image_filenames):
+def make_image_data_list(image_filename):
     """
-    image_filenames is a list of filename strings
+    image_filename is a list of filename strings
     Returns a list of dicts formatted as the Vision API
         needs them to be
     """
     img_requests = []
-    for imgname in image_filenames:
+    for imgname in image_filename:
         with open(imgname, 'rb') as f:
             ctxt = b64encode(f.read()).decode()
             img_requests.append({
@@ -110,30 +104,17 @@ def make_image_data_list(image_filenames):
             })
     return img_requests
 
-def make_image_data(image_filenames):
+def make_image_data(image_filename):
     """Returns the image data lists as bytes"""
-    imgdict = make_image_data_list(image_filenames)
+    imgdict = make_image_data_list(image_filename)
     return json.dumps({"requests": imgdict }).encode()
 
-def request_ocr(api_key, image_filenames):
+def request_ocr(api_key, image_filename):
     response = requests.post(ENDPOINT_URL,
-                             data=make_image_data(image_filenames),
+                             data=make_image_data(image_filename),
                              params={'key': api_key},
                              headers={'Content-Type': 'application/json'})
     return response
-
-def request_search(api_key):
-
- response = requests.post(ENDPOINT_URL,
-                          data=make_image_data(image_filenames),
-                          params={'key': api_key},
-                          headers={'Content-Type': 'application/json'})
- return response
-
-
-
-
-
 
 
 
