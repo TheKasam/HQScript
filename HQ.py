f5691499d1e491d085f05920651ea55b378c047b
Venu Kasam <Venu@Venus-MacBook-Pro.local>
2017-12-21 23:11:54 -0600

    mvp done

--- HQ.py
+++ HQ.py
@@ -8,6 +8,7 @@ import json
 import requests
 import bs4 as bs
 from PIL import Image
+import pyscreenshot as ImageGrab
 
 
 ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
@@ -15,77 +16,115 @@ RESULTS_DIR = 'jsons'
 makedirs(RESULTS_DIR, exist_ok=True)
 
 def main():
-    get_text()
-
+    question, answers = get_text()
+    ans_method_one(question, answers)
+    ans_method_two(question, answers)
+    ans_method_three(question, answers)
 
     end = time.time()
     print(end - start)
     #print(ans.json()['queries']['request'])
 
-def get_ans(question, answers):
+def ans_method_one(question, answers):
 
-    search_key = 'AIzaSyBqcHbDxpT8KGF1dEC7glg5dq2b2H7jn7o'
-    search_id = '016671866865682481259:ivh1ljytmsm'
     q = 'what is the epipremnum aureum house plant known as?'
-    ans = requests.get('https://www.googleapis.com/customsearch/v1?key='+search_key+'&cx='+search_id+'&q='+q)
-    #print('https://www.googleapis.com/customsearch/v1?key='+search_key+'&cx='+search_id+'&q='+q)
     questionWord = question.strip().split(" ")[0]
 
     #other method is to get request with actual url and use bs4 to locate the answer google gives a
-    if questionWord == 'What' or questionWord == "Of":
-        googleResult = requests.get('https://www.google.com/search?q='+q)
+    if True: #questionWord == 'What' or questionWord == "of":
+        googleResult = requests.get('https://www.google.com/search?q='+question)
 
         soup = bs.BeautifulSoup(googleResult.text,'lxml')
-
         if soup != None:
             headText = soup.find('div',{'class':'_sPg'})
-            headText = str(headText.text).split(" ")
+            headText = headText.text
+            headText = headText.lower()
+            #print(headText)
+            headText = headText.replace(" a ","")
+            headText = headText.replace(" i ","")
+            headText = headText.replace("."," ")
+            headText = headText.split(" ")
             freqDict = {}
+
             for x in answers:
                 freqDict[x] = 0
 
             for x in headText:
-                if answers[0] in x:
+                x = x.strip(',')
+                print(x,end=" ")
+                if x == answers[0]:
                     freqDict[answers[0]] += 1
-                elif answers[1] in x:
+                elif x == answers[1] :
                     freqDict[answers[1]] += 1
-                elif answers[2] in x:
+                elif  x == answers[2]:
                     freqDict[answers[2]] += 1
-            print(headText)
             print()
             print(freqDict)
+
+
+def ans_method_two(question,answers):
+    print()
+    print("Number of results for each option:")
+    for ans in answers:
+        question = question + " " + ans
+        googleResult = requests.get('https://www.google.com/search?q='+question)
+        soup = bs.BeautifulSoup(googleResult.text,'lxml')
+
+        results_num = soup.find('div',{'id':'resultStats'})
+        print(ans + ": " +results_num.text.split(" ")[1])
+
+    # search_key = 'AIzaSyBqcHbDxpT8KGF1dEC7glg5dq2b2H7jn7o'
+    # search_id = '016671866865682481259:ivh1ljytmsm'
+    # #test question 1: 'what is the epipremnum aureum house plant known as?'
+    #
+    # for ans in answers:
+    #     q = question + " " + ans
+    #
+    #     queryAnswer = requests.get('https://www.googleapis.com/customsearch/v1?key='+search_key+'&cx='+search_id+'&q='+q)
+    #     print('https://www.googleapis.com/customsearch/v1?key='+search_key+'&cx='+search_id+'&q='+q)
+    #     print(ans + ":  ")
+    #     print(queryAnswer.json()['queries']['request'][0]['totalResults'])
+
+def ans_method_three(question,answers):
+
+    print()
+    print("Number of times each option appears on page")
+    googleResult = requests.get('https://www.google.com/search?q='+question)
+    soup = bs.BeautifulSoup(googleResult.text,'lxml')
+    results_num = soup.find('div',{'id':'search'})
+
+    for ans in answers:
+        print(ans + ": " + str(results_num.text.lower().count(ans)))
+
 
 
 def get_text():
     api_key = 'AIzaSyD62V5CUucbPUnx21i-cQvKS9cOngm2eeI'
     image_filename = ['imageTest.png']
-    crop_image(image_filename[0])
+    #crop_image(image_filename[0])
     if not api_key or not image_filename:
-        print("""
-            Please supply an api key, then one or more image filenames
-            $ python cloudvisreq.py api_key image1.jpg image2.png""")
+        print("missing key or file name")
     else:
+        print("a")
         response = request_ocr(api_key, image_filename)
+        print("b")
         if response.status_code != 200 or response.json().get('error'):
-            print(response.text)
+            print('error no internet connection?')#response.text to see errro
         else:
             for idx, resp in enumerate(response.json()['responses']):
 
-
                 # print the plaintext to screen for convenience
-                t = resp['textAnnotations'][0]
-                print()
-                textLst = t['description'].split("\n")
-                print(textLst)
-                textLst = textLst[2:]
+                text = resp['textAnnotations'][0]['description']
+                textLst = text.lower().split("\n")
 
-                question = textLst[0] + " "+textLst[1]
-                #print(question)
-
-                answers = textLst[2:5]
-                #print(answers)
+                question = textLst[2] + " "+textLst[3]
                 print()
-                get_ans(question, answers)
+                print(question)
+                print()
+                answers = textLst[4:7]
+                print(answers)
+                print()
+                return(question, answers)
 
 def make_image_data_list(image_filename):
     """
